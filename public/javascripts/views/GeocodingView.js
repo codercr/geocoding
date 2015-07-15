41280fd4f6e405ea4452eabbcac588c73bc8a6da
Carlos Reyna <creyna@ingen10.biz>
2015-07-15 11:38:12 -0700

    Add GeocodingView to start dom visualization (Basic ugly DOM render)

--- public/javascripts/views/GeocodingView.js
+++ public/javascripts/views/GeocodingView.js
@@ -1,19 +1,99 @@
 (function () {
-    var GeocodingView = function GeocodingView(attachPoint, origin, destinations) {
+    var createSortButtonCallback = function createSortButtonCallback(geocodingView) {
+            return function () {
+                geocodingView._sortButtonClicked();
+            }
+        },
+        createGeocodeCallback = function createGeocodeCallback(geocodingView) {
+            return function (geocoding) {
+                geocodingView._geocoding = geocoding;
+                geocodingView._renderSorted();
+            }
+        },
+        GeocodingView = function GeocodingView(attachPoint, origin, destinations) {
 
-        if (!(typeof attachPoint === "object" && attachPoint instanceof Element)) {
-            throw new TypeError("Parameter attachPoint requires a DOM element");
+            if (!(typeof attachPoint === "object" && attachPoint instanceof Element)) {
+                throw new TypeError("Parameter attachPoint requires a DOM element");
+            }
+
+            if (!(typeof origin === "string")) {
+                throw new TypeError("Parameter origin requires a string");
+            }
+
+            if (!(typeof destinations === "object" && destinations instanceof Array)) {
+                throw new TypeError("Parameter destinations requires an array");
+            }
+
+            this._origin = origin;
+            this._destinations = destinations;
+
+            // Unsorted Origin Address Node
+            this._unsortedOriginNode = attachPoint.querySelector(".unsorted .origin .addresses");
+
+            // Unsorted Destinations Address Node
+            this._unsortedDestinationsNode = attachPoint.querySelector(".unsorted .destinations .addresses");
+
+            // Sorted Destinations Address Node
+            this._sortedDestinationsNode = attachPoint.querySelector(".sorted .destinations .addresses");
+
+            // sortButton Node
+            this._sortButtonNode = attachPoint.querySelector(".sortButton button");
+
+            // Add sortButon click event listener
+            this._sortButtonNode.addEventListener("click", createSortButtonCallback(this));
+
+            this._renderUnsorted();
+
+            return this;
+        };
+
+    GeocodingView.prototype._renderUnsorted = function _renderUnsorted() {
+        this._createAndAppendUnsortedOrigin();
+        this._createAndAppendUnsortedDestinations();
+    };
+
+    GeocodingView.prototype._createAndAppendUnsortedOrigin = function _createAndAppendUnsortedOrigin() {
+        var addressElement = this._createAddressElement(this._origin);
+        this._unsortedOriginNode.appendChild(addressElement);
+    };
+
+    GeocodingView.prototype._createAndAppendUnsortedDestinations = function _createAndAppendUnsortedDestinations() {
+        var destinations = this._destinations,
+            addressElement, i;
+
+        for (i = 0; i < destinations.length; i++) {
+            addressElement = this._createAddressElement(destinations[i]);
+            this._unsortedDestinationsNode.appendChild(addressElement);
         }
 
-        if (!(typeof origin === "string")) {
-            throw new TypeError("Parameter origin requires a string");
+    };
+
+    GeocodingView.prototype._sortButtonClicked = function _sortButtonClicked() {
+        // Geocoding library
+        new Geocoding(this._origin, this._destinations, createGeocodeCallback(this));
+    };
+
+    GeocodingView.prototype._renderSorted = function _renderSorted() {
+        this._createAndAppendSortedDestinations();
+    };
+
+    GeocodingView.prototype._createAndAppendSortedDestinations = function _createAndAppendSortedDestinations() {
+        var destinations = this._geocoding.destinationsOrderedByDistance(),
+            addressElement, i;
+
+        for (i = 0; i < destinations.length; i++) {
+            addressElement = this._createAddressElement(destinations[i]);
+            this._sortedDestinationsNode.appendChild(addressElement);
         }
 
-        if (!(typeof destinations === "object" && destinations instanceof Array)) {
-            throw new TypeError("Parameter destinations requires an array");
-        }
+    };
 
-        return this;
+    GeocodingView.prototype._createAddressElement = function _createAddressElement(address) {
+        var addressElement = document.createElement("div");
+
+        addressElement.innerHTML = address;
+
+        return addressElement;
     };
 
     window.GeocodingView = GeocodingView;
--- spec/javascripts/views/GeocodingViewSpec.js
+++ spec/javascripts/views/GeocodingViewSpec.js
@@ -50,4 +50,53 @@ describe("GeocodingView", function () {
             });
         });
     });
+
+    describe("#renderUnsorted", function () {
+        it("renders unsorted content", function () {
+            var unsortedOrigin = attachPoint.querySelector(".unsorted .origin .addresses"),
+                unsortedDestinations = attachPoint.querySelector(".unsorted .destinations .addresses");
+
+            expect(unsortedOrigin.childNodes.length).toEqual(0);
+            expect(unsortedDestinations.childNodes.length).toEqual(0);
+
+            new GeocodingView(attachPoint, GoogleMapsHelper.origin, GoogleMapsHelper.destinations);
+
+            expect(unsortedOrigin.childNodes.length).toEqual(1);
+            expect(unsortedOrigin.childNodes[0].innerHTML).toEqual("510 Victoria, Venice, CA");
+            expect(unsortedDestinations.childNodes.length).toEqual(7);
+            expect(unsortedDestinations.childNodes[0].innerHTML).toEqual("Times Square, Manhattan, NY 10036");
+            expect(unsortedDestinations.childNodes[1].innerHTML).toEqual("13000 S Dakota 244, Keystone, SD 57751");
+            expect(unsortedDestinations.childNodes[2].innerHTML).toEqual("1600 Pennsylvania Ave NW, Washington, DC 20500");
+            expect(unsortedDestinations.childNodes[3].innerHTML).toEqual("Golden Gate Bridge, San Francisco, CA 94129");
+            expect(unsortedDestinations.childNodes[4].innerHTML).toEqual("Stonehenge, A344, Amesbury, Wiltshire SP4 7DE, United Kingdom");
+            expect(unsortedDestinations.childNodes[5].innerHTML).toEqual("Great Wall of China");
+            expect(unsortedDestinations.childNodes[6].innerHTML).toEqual("Hollywood Sign, Los Angeles, CA");
+        });
+    });
+
+    describe("#renderSorted", function () {
+        it("renders sorted content", function () {
+            var sortedDestinations = attachPoint.querySelector(".sorted .destinations .addresses"),
+                geocodingView;
+
+            spyOn(Geocoding.Location.prototype, "computeDistanceBetween").and.callFake(function (location) {
+                return GoogleMapsHelper.distances[location.location()];
+            });
+
+            expect(sortedDestinations.childNodes.length).toEqual(0);
+
+            geocodingView = new GeocodingView(attachPoint, GoogleMapsHelper.origin, GoogleMapsHelper.destinations);
+            geocodingView._sortButtonClicked();
+
+            expect(sortedDestinations.childNodes.length).toEqual(7);
+            expect(sortedDestinations.childNodes[0].innerHTML).toEqual("Hollywood Sign, Los Angeles, CA");
+            expect(sortedDestinations.childNodes[1].innerHTML).toEqual("Golden Gate Bridge, San Francisco, CA 94129");
+            expect(sortedDestinations.childNodes[2].innerHTML).toEqual("13000 S Dakota 244, Keystone, SD 57751");
+            expect(sortedDestinations.childNodes[3].innerHTML).toEqual("1600 Pennsylvania Ave NW, Washington, DC 20500");
+            expect(sortedDestinations.childNodes[4].innerHTML).toEqual("Times Square, Manhattan, NY 10036");
+            expect(sortedDestinations.childNodes[5].innerHTML).toEqual("Stonehenge, A344, Amesbury, Wiltshire SP4 7DE, United Kingdom");
+            expect(sortedDestinations.childNodes[6].innerHTML).toEqual("Great Wall of China");
+
+        });
+    });
 });
